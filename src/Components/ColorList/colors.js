08ea4ecb04ade20c88eae2a0a198d71b1196a41c
zahr2-lab <zahr2.mobarak@gmail.com>
2020-09-25 09:56:54 +0330

    1 file modified

--- src/Components/ColorList/colors.js
+++ src/Components/ColorList/colors.js
@@ -1,11 +1,11 @@
 colors = [
 
   {
-    name : 'Roseanna',
+    name : 'Piglet',
     color: 'white',
     deg : '10',
-    grad_1: 'ffc3a0',
-    grad_2: 'ffafbd'
+    grad_1: 'ee9ca7',
+    grad_2: 'ffdde1'
   },{
     name : 'Almost',
     color: 'white',
@@ -13,10 +13,88 @@ colors = [
     grad_1: 'ddd6f3',
     grad_2: 'faaca8'
   },{
-    name : 'Piglet',
+    name : 'Roseanna',
     color: 'white',
     deg : '10',
-    grad_1: 'ee9ca7',
-    grad_2: 'ffdde1'
-  }
+    grad_1: 'ffc3a0',
+    grad_2: 'ffafbd'
+  },{
+    name : 'Tranquil',
+    color: 'white',
+    deg : '10',
+    grad_1: 'ef629f',
+    grad_2: 'eecda3'
+  },{
+    name : 'Sha La La',
+    color: 'white',
+    deg : '10',
+    grad_1: 'd66d75',
+    grad_2: 'e29587'
+  },{
+    name : 'Sunset',
+    color: 'white',
+    deg : '10',
+    grad_1: 'ff7e5f',
+    grad_2: 'feb47b'
+  },{
+    name : 'Sweet Morning',
+    color: 'white',
+    deg : '10',
+    grad_1: 'ff5f6d',
+    grad_2: 'ffc371'
+  },{
+    name : 'Bourbon',
+    color: 'white',
+    deg : '10',
+    grad_1: 'ec6f66',
+    grad_2: 'f3a183'
+  },{
+    name : 'Orange Coral',
+    color: 'white',
+    deg : '10',
+    grad_1: 'ff9966',
+    grad_2: 'ff5e62'
+  },{
+    name : 'Pinky',
+    color: 'white',
+    deg : '10',
+    grad_1: 'dd5e89',
+    grad_2: 'f7bb97'
+  },{
+    name : 'Pale Wood',
+    color: 'white',
+    deg : '10',
+    grad_1: 'eacda3',
+    grad_2: 'd6ae7b'
+  },{
+    name : 'Pinky',
+    color: 'white',
+    deg : '10',
+    grad_1: 'dd5e89',
+    grad_2: 'f7bb97'
+  },{
+    name : 'A Lost Memory',
+    color: 'white',
+    deg : '10',
+    grad_1: 'de6262',
+    grad_2: 'ffb88c'
+  },{
+    name : 'Purple Love',
+    color: 'white',
+    deg : '10',
+    grad_1: 'cc2b5e',
+    grad_2: '753a88'
+  },{
+    name : 'Kashmir',
+    color: 'white',
+    deg : '10',
+    grad_1: '614385',
+    grad_2: '516395'
+  },{
+    name : 'Mauve',
+    color: 'white',
+    deg : '10',
+    grad_1: '42275a',
+    grad_2: '734b6d'
+  },
 ]
