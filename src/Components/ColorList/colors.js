609a0d36b5a6264bfefb6a4d76d2f52702eddba3
zahr2-lab <zahr2.mobarak@gmail.com>
2020-09-25 09:19:23 +0330

    4 files modified

--- src/Components/ColorList/colors.js
+++ src/Components/ColorList/colors.js
@@ -7,6 +7,12 @@ colors = [
     grad_1: 'ffc3a0',
     grad_2: 'ffafbd'
   },{
+    name : 'Almost',
+    color: 'white',
+    deg : '10',
+    grad_1: 'ddd6f3',
+    grad_2: 'faaca8'
+  },{
     name : 'Piglet',
     color: 'white',
     deg : '10',
