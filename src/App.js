8cf7432812d1918e149312c37349b16a87895187
zahr2-lab <zahr2.mobarak@gmail.com>
2020-09-25 08:58:47 +0330

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./style.css";
-import ColorList from './Components/ColorList/ColorList'
+import ColorList from './Components/ColorList/ColorList';
+import Search from './Components/Search/Search'
+
 
 export default function App() {
   return (
     <div>
-      <h1>Search a Color</h1>
+      <h1> &#9735; Search a Color &#9735;</h1>
+      <Search  />
       <ColorList />
     </div>
   );
