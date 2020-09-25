f138e1c429da704c0cc6fe31ea0d0d8aa619d718
zahr2-lab <zahr2.mobarak@gmail.com>
2020-09-25 11:04:52 +0330

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import ColorList from './Components/ColorList/ColorList';
 import Search from './Components/Search/Search'
 
 
 export default function App() {
+  const [name, setName] = useState('');
+
   return (
     <div id='container'>
       <h1> &#9735; Search a Color &#9735;</h1>
-      <Search  />
-      <ColorList />
+      <Search name={name} setName={setName}/>
+      <ColorList name={name} />
     </div>
   );
 }
